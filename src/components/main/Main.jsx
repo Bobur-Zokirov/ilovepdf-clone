6cea1bc0bd6152a1624c09ec97d8009c66a8b0e0
Bobur <zeromegamaks@gmail.com>
2022-10-22 11:27:47 -0700

    first commit header added

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -4,7 +4,12 @@ import "./main.css";
 
 const Main = () => {
   return (
-    <div className="main">Main</div>
+    <div className="main">
+      <h1>
+        Top menu must be fixed and <br/><hr/>
+        Main must be added
+      </h1>
+    </div>
   )
 }
 
